f8192abad37b9c78147c19299f87a4bebf80aa67
Gahyeon19 <wjsrkgus0119@naver.com>
2024-09-30 15:48:08 +0900

    App6

---
+++ counterApp/src/component/Components.jsx
@@ -0,0 +1,26 @@
+export function Func1() {
+  return (
+    <>
+      <h1>Func1</h1>
+    </>
+  )
+}
+
+export function Func2() {
+  return (
+    <>
+      <h1>Func2</h1>
+      
+    </>
+  )
+}
+
+export function Func3() {
+  return (
+    <>
+      <h1>Func3</h1>
+    </>
+  )
+}
+
+export default Func3
